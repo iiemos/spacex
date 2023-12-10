a8501ba3424e7a884b939d6f57d0a2e0b2a932b4
ason <2254749998@qq.com>
2023-12-10 20:51:08 +0800

    123456

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  assetsInclude: ['src/assets/**'],
+  // assetsInclude: ['src/assets/**'],
   plugins: [
     vue(),
   ],
